1348460102b7324c88f07a1adb1c3ae456140cd9
David Lucas <davidalucas92@gmail.com>
2024-01-30 12:46:27 -0500

    Adds test with real data

--- test/test_trebuchet.py
+++ test/test_trebuchet.py
@@ -10,3 +10,7 @@ def test_parse_cal_value_returns_expected():
 
 def test_sum_all_cal_values_works_with_example_data():
     assert sum_all_cal_values("data/day_1/example_1.txt") == 142
+
+
+def test_sum_all_cal_values_works_with_real_data():
+    assert sum_all_cal_values("data/day_1/data.txt") == 55477
