c5f404f1fc789bc37707c3cf44431f1ceb12078c
David Lucas <davidalucas92@gmail.com>
2024-02-01 15:09:26 -0500

    Adds tests for the is_valid method

--- test/test_cube_game.py
+++ test/test_cube_game.py
@@ -12,3 +12,23 @@ def test_from_string_constructs_correctly():
 
     assert actual.id == expected.id
     assert actual.reveals == expected.reveals
+
+
+def test_is_valid_returns_false_when_expected():
+    cube_game = CubeGame.from_string(
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+    )
+
+    valid_dict = {"red": 12, "green": 13, "blue": 14}
+
+    assert_that(cube_game.is_valid(valid_dict)).is_false()
+
+
+def test_is_valid_returns_true_when_expected():
+    cube_game = CubeGame.from_string(
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+    )
+
+    valid_dict = {"red": 12, "green": 13, "blue": 14}
+
+    assert_that(cube_game.is_valid(valid_dict)).is_true()
